3dca0ac4e116ea6b4995276b5aeb52a2cbcaef6b
Your Name <you@example.com>
2021-03-10 14:48:02 -0500

    We have debuged our proyect in Udebug and the expected output was equal

--- src/ui/ExactSum.java
+++ src/ui/ExactSum.java
@@ -7,8 +7,6 @@ public class ExactSum {
     private static List<int[]> cases = new ArrayList<int[]>();
     private static List<Integer> money = new ArrayList<Integer>();
     private static final String SPACE = " ";
-    private static int bestPrice1 = 0;
-    private static int bestPrice2 = 1000000;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,9 +18,10 @@ public class ExactSum {
     }
 
     public static void readConsole(BufferedReader br) throws IOException {
-        String nextLine = ";";
-        while (br.readLine() != null || nextLine != null) {
-            int size = Integer.parseInt(br.readLine());
+        boolean render = false;
+        String line = br.readLine();
+        while (!render) {
+            int size = Integer.parseInt(line);
             int[] booksPrices = new int[size];
             String[] parts = br.readLine().split(SPACE);
             for (int i = 0; i < booksPrices.length; i++) {
@@ -30,13 +29,35 @@ public class ExactSum {
             }
             cases.add(booksPrices);
             money.add(Integer.parseInt(br.readLine()));
-            nextLine = br.readLine();
+            String nextLine = br.readLine();
+            line = br.readLine();
+            render = (nextLine.equals("") && line.equals("")) ? true : false;
         }
 
     }
 
     public static void WriteSolution(BufferedWriter bw) throws IOException {
+        sortBooksPrices(cases);
+        for (int i = 0; i < cases.size(); i++) {
+            bw.write("Case " + (i + 1) + ":" + getBestPricesForCase(cases.get(i), money.get(i)));
+            bw.flush();
+        }
+    }
 
+    public static String getBestPricesForCase(int[] p, int money) {
+        int bestPrice1 = 0;
+        int bestPrice2 = 1000000;
+        for (int i = 0; i < p.length; i++) {
+            int priceBook1 = p[i];
+            int priceBook2 = searchBinary(i + 1, p.length - 1, (money - priceBook1), p);
+            if (priceBook2 > -1) {
+                if (Math.abs(priceBook2 - priceBook1) < Math.abs(bestPrice2 - bestPrice1)) {
+                    bestPrice1 = priceBook1;
+                    bestPrice2 = priceBook2;
+                }
+            }
+        }
+        return " Peter should buy books whose prices are " + bestPrice1 + " and " + bestPrice2 + ".\n\n";
     }
 
     public static void sortBooksPrices(List<int[]> prices) {
